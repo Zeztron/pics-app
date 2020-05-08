45bbaaa2cf93dba9ea2f8a6880d17f45597c8193
unknown <pateltharsh@gmail.com>
2020-05-08 15:26:55 -0400

    added http to get images and adding image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'src/app.dart' show App;
 
 void main() => runApp(App());
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => new _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  Future<void> _fetchImage() async {
+    counter++;
+    final response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() => images.add(imageModel));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(
-            Icons.add,
+            Icons.add_a_photo,
             semanticLabel: 'Add an image',
           ),
-          onPressed: () {
-            print('Hi there');
-          },
+          onPressed: _fetchImage
         ),
         appBar: AppBar(
           title: Text('Lets see some images!'),
